ca0f691059ff3b2b867c0c5e4c39dee9d739c445
Dennis Yaremenko <yaremdennis@gmail.com>
2024-11-25 11:05:32 +0200

    Initial Commit

---
+++ EssentialFeedManually/EssentialFeedManually.h
@@ -0,0 +1,18 @@
+//
+//  EssentialFeedManually.h
+//  EssentialFeedManually
+//
+//  Created by Denis Yaremenko on 25.11.2024.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for EssentialFeedManually.
+FOUNDATION_EXPORT double EssentialFeedManuallyVersionNumber;
+
+//! Project version string for EssentialFeedManually.
+FOUNDATION_EXPORT const unsigned char EssentialFeedManuallyVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <EssentialFeedManually/PublicHeader.h>
+
+
